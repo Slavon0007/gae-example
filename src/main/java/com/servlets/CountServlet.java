dfa41455baca6f8ace63b1735b39fad3b2497a1a
Slava <slavkaklimov@gmail.com>
2018-10-10 08:11:10 +0300

    count

--- src/main/java/com/servlets/CountServlet.java
+++ src/main/java/com/servlets/CountServlet.java
@@ -19,42 +19,34 @@ public class CountServlet extends HttpServlet {
 
 
     @Override
-    public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        Cookie cookie = new Cookie("Some_id","1");
 
-        cookie.setMaxAge(30);
+        int value;
 
-        response.addCookie(cookie);
-
-        VelocityContext context = new VelocityContext();
-        Template template;
-
-        try {
-            //создание обьекта типа Template с помощью метода getTemplate библиотеки Velocity который
-            //в качестве аргумента принимает параметр типа String, который яляется путем к файлу шаблона
-            template = Velocity.getTemplate("templates/countServlet.html");
-            template.merge(context, response.getWriter());
-        } catch (ResourceNotFoundException e) {
-            response.getWriter().write(e.getMessage());
-        } catch (ParseErrorException pee) {
-            response.getWriter().write(pee.getMessage());
-        } catch (MethodInvocationException e) {
-            response.getWriter().write(e.getMessage());
+        Cookie[] cookies = request.getCookies(); //я взял с клиента значение куки
+        if (cookies == null) {//если значения пустые создаю новый масивв куки и записываю новое куки
+            Cookie cookie = new Cookie("a", "0");
+            cookie.setMaxAge(30);
+            cookies = new Cookie[]{cookie};
+            response.addCookie(cookies[0]); // я ложу ноль что бы получить следующее значение
+        } else {                            //циклом фор перебераю масив
+            for(Cookie cookie:cookies) {
+                value = Integer.parseInt(cookie.getValue());  // в переменную записываю значения куки и перевожу в инт
+                value++;                                    // делаю инкрементацию
+                cookie.setValue(String.valueOf(value));     //записываю в куки значения инкримента и перевожу в стринг
+                response.getWriter().write(String.valueOf(value)); // вывожу значения на сервер в стринг
+                response.addCookie(cookie);                  // ложу куки клиенту
+            }
         }
 
+       // response.setContentType("text/html");
 
     }
 
     @Override
-    public  void  doPost(HttpServletRequest request,HttpServletResponse response)throws ServletException, IOException{
-        Cookie[] cookies = request.getCookies();
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        PrintWriter pw = response.getWriter();
-
-         for(Cookie cookie:cookies){
-                pw.println(cookie.getValue()+1);
-        }
 
     }
 
